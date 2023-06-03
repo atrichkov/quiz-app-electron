5d678b145e475e55ad223beb25846185ec07dd11
Asen <asen.trichkov1@gmail.com>
2023-06-04 02:18:07 +0300

    feat: load questions from main process

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const { app, BrowserWindow, protocol, ipcMain } = require('electron');
 const path = require('path');
 const axios = require('axios');
+require('dotenv').config();
+
+const isDev = process.env.NODE_ENV === 'development';
+const isMac = process.platform === 'darwin';
+const apiKey = '<YOUR_API_KEY>';
+const questionsCount = 2;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
         width: 1420,
         height: 960,
         webPreferences: {
@@ -13,25 +20,106 @@ const createWindow = () => {
         },
     });
 
-    const response = axios.get('https://catfact.ninja/fact');
-    response.then((res) => {
-        win.loadFile('app/templates/index.html');
-    });
-    win.webContents.openDevTools();
+    mainWindow.loadFile(path.join(__dirname, './renderer/pages/index.html'));
+    ipcMain.on('fetch-data', async (event, args) => {
+        try {
+           const response = await axios.get(`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${questionsCount}`);
+            // questionsArr = [
+            //     {
+            //         id: 1,
+            //         question: 'How to delete a directory in Linux?',
+            //         description:
+            //             'The rmdir commands deletes only empty directories. To delete a folder recursively use the rm -rf command.',
+            //         answers: {
+            //             answer_a: 'ls',
+            //             answer_b: 'delete',
+            //             answer_c: 'remove',
+            //             answer_d: 'rmdir',
+            //             answer_e: null,
+            //             answer_f: null,
+            //         },
+            //         multiple_correct_answers: 'false',
+            //         correct_answers: {
+            //             answer_a_correct: 'false',
+            //             answer_b_correct: 'false',
+            //             answer_c_correct: 'false',
+            //             answer_d_correct: 'true',
+            //             answer_e_correct: 'false',
+            //             answer_f_correct: 'false',
+            //         },
+            //         correct_answer: 'answer_d',
+            //         explanation: 'rmdir deletes an empty directory',
+            //         tip: null,
+            //         tags: [],
+            //         category: 'Linux',
+            //         difficulty: 'Easy',
+            //     },
+            //     {
+            //         id: 957,
+            //         question: 'At its core, Kubernetes is a platform for:',
+            //         description: null,
+            //         answers: {
+            //             answer_a:
+            //                 'Provisioning machines (similar to Puppet, Ansible)',
+            //             answer_b:
+            //                 'Running and scheduling container applications on a cluster',
+            //             answer_c: 'Packaging software in containers',
+            //             answer_d: null,
+            //             answer_e: null,
+            //             answer_f: null,
+            //         },
+            //         multiple_correct_answers: 'false',
+            //         correct_answers: {
+            //             answer_a_correct: 'false',
+            //             answer_b_correct: 'true',
+            //             answer_c_correct: 'false',
+            //             answer_d_correct: 'false',
+            //             answer_e_correct: 'false',
+            //             answer_f_correct: 'false',
+            //         },
+            //         correct_answer: null,
+            //         explanation: null,
+            //         tip: null,
+            //         tags: [
+            //             {
+            //                 name: 'Kubernetes',
+            //             },
+            //         ],
+            //         category: 'DevOps',
+            //         difficulty: 'Easy',
+            //     },
+            // ];
+            
+            event.reply('fetch-data-response', response.data);
+            //     });
+            // });
+            // request.on('finish', () => {
+            //     console.log('Request is Finished')
+            // });
+            // request.on('abort', () => {
+            //     console.log('Request is Aborted')
+            // });
+            // request.on('error', (error) => {
+            //     console.log(`ERROR: ${JSON.stringify(error)}`)
+            // });
+            // request.on('close', (error) => {
+            //     console.log('Last Transaction has occurred')
+            // });
+            // request.setHeader('Content-Type', 'application/json');
+            // request.end();
+        } catch (error) {
+
+            console.log(error);
+           console.error(error);
+        }
+     });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 };
 
 app.whenReady().then(() => {
-    // Create custom protocol for local media loading
-    protocol.registerFileProtocol('media-loader', (request, callback) => {
-        const url = request.url.replace('media-loader://', '');
-        try {
-            return callback(url);
-        } catch (err) {
-            console.error(error);
-            return callback(404);
-        }
-    });
-
     createWindow();
 
     app.on('activate', () => {
@@ -40,7 +128,7 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+    if (!isMac) {
         app.quit();
     }
 });
---
+++ renderer/js/index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const { ipcRenderer } = require('electron');
+const jQuery = require('jquery');
+
+document.addEventListener('DOMContentLoaded', () => {
+    jQuery('#startBtn').on('click', function (e) {
+        ipcRenderer.on('fetch-data-response', (event, data) => {
+            const questionsArr = data; // JSON.parse(Buffer.from(data).toString('utf8'));
+
+            jQuery('#startBtn').addClass('animated fadeOutUp');
+            jQuery('.quiz').addClass('animated fadeInDown').removeClass('hidden');
+            
+            jQuery('form')
+                .addClass('animated fadeInDown')
+                .prepend(parseQuestion(questionsArr[0], 0));
+
+            let correctAnswersCount = 0;
+            function handleAnswerBtnClick() {
+                jQuery('.btnQ').on('click', function (e) {
+                    const selectedAnswer = jQuery(this).attr('for');
+                    let index = jQuery(this)
+                        .parent()
+                        .parent()
+                        .data('index');
+
+                    if (
+                        questionsArr[index].correct_answers[
+                            selectedAnswer + '_correct'
+                        ] === 'true'
+                    )
+                        correctAnswersCount++;
+                    jQuery(this)
+                        .parent()
+                        .parent()
+                        .addClass('animated fadeOutUp')
+                        .hide();
+                    const nextQuestionIndex = ++index;
+
+                    circle.animate(nextQuestionIndex / questionsArr.length);
+
+                    if (questionsArr[nextQuestionIndex]) {
+                        jQuery('form')
+                            .addClass('animated fadeInDown')
+                            .prepend(
+                                parseQuestion(
+                                    questionsArr[nextQuestionIndex],
+                                    nextQuestionIndex
+                                )
+                            );
+                        handleAnswerBtnClick();
+                    } else {
+                        jQuery('.q' + index).addClass('animated fadeOutUp');
+                        jQuery('.quiz').append(
+                            `<div class="end animated bounceInDown">Results: (${correctAnswersCount} / ${questionsArr.length})</div>`
+                        );
+                    }
+                });
+            }
+            handleAnswerBtnClick();
+        });
+
+        ipcRenderer.send('fetch-data'); 
+
+        function parseQuestion(question, index) {
+            return `<fieldset data-index=${index} class="q q${index}">
+                    <div class="content">
+                        <legend>1. ${question.question}</legend>
+                        <input type="radio" name="q${index}" id="${question.answers.answer_a}" class="btnQ">
+                            <label for="answer_a" class="btnQ"><span>${question.answers.answer_a}</span></label>
+                        <input type="radio" name="q${index}" id="${question.answers.answer_b}">
+                            <label for="answer_b" class="btnQ"><span>${question.answers.answer_b}</span></label>
+                        <input type="radio" name="q${index}" id="${question.answers.answer_c}">
+                            <label for="answer_c" class="btnQ"><span>${question.answers.answer_c}</span></label>
+                        <input type="radio" name="q${index}" id="${question.answers.answer_d}">
+                            <label for="answer_d" class="btnQ"><span>${question.answers.answer_d}</span></label>
+                    </div>
+                    <div class="button-space">
+
+                    </div>
+                </fieldset>`;
+        }
+    });
+    var circle = new ProgressBar.Circle('#circle-container', {
+        duration: 200,
+        strokeWidth: 5,
+        trailWidth: 5,
+        trailColor: '#ddd',
+        from: {
+            color: '#218CCC',
+        },
+        to: {
+            color: '#047E3C',
+        },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+        },
+    });
+});
