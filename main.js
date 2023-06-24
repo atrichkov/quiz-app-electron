f8bd8c3e0ba37b81ad3d0ea84490b589fd174a73
Asen <asen.trichkov1@gmail.com>
2023-06-25 00:48:00 +0300

    add background and refactoring

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { app, BrowserWindow, ipcMain, screen } = require('electron');
 const fs = require('fs');
 const path = require('path');
@@ -10,64 +12,62 @@ const apiKey = process.env.API_KEY || '<YOUR_API_KEY>';
 const questionsCount = 5;
 
 const createWindow = (width, height) => {
-  const mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: Number(((75 / 100) * width).toFixed()),
-    height: Number(((75 / 100) * height).toFixed()),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      // preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+    const mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+        width: Number(((75 / 100) * width).toFixed()),
+        height: Number(((75 / 100) * height).toFixed()),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            // preload: path.join(__dirname, 'preload.js'),
+        },
+    });
 
-  mainWindow.loadFile(path.join(__dirname, './renderer/pages/index.html'));
-  ipcMain.on('fetch-data', async (event, args) => {
-    try {
-      let questionsArr;
-      if (isDev) {
-        // use dummy data to save api calls
-        const data = fs.readFileSync('./tests/data.json', {
-          encoding: 'utf8',
-          flag: 'r',
-        });
-        questionsArr = JSON.parse(data);
-      } else {
-        const response = await axios.get(
-          `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${questionsCount}`,
-        );
-        questionsArr = response.data;
-      }
+    mainWindow.loadFile(path.join(__dirname, './renderer/pages/index.html'));
+    ipcMain.on('fetch-data', async (event, args) => {
+        try {
+            let questionsArr;
+            if (isDev) {
+                // use dummy data to save api calls
+                const data = fs.readFileSync('./tests/data.json', {
+                    encoding: 'utf8',
+                    flag: 'r',
+                });
+                questionsArr = JSON.parse(data);
+            } else {
+                const response = await axios.get(`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${questionsCount}`);
+                questionsArr = response.data;
+            }
 
-      event.reply('fetch-data-response', questionsArr);
-    } catch (error) {
-      console.error(error);
+            event.reply('fetch-data-response', questionsArr);
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
     }
-  });
 
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
-
-  ipcMain.on('reload', () => mainWindow.webContents.reload());
+    ipcMain.on('reload', () => mainWindow.webContents.reload());
 };
 
 app.whenReady().then(() => {
-  const primaryDisplay = screen.getPrimaryDisplay();
-  const { width, height } = primaryDisplay.workAreaSize;
-  createWindow(width, height);
+    const primaryDisplay = screen.getPrimaryDisplay();
+    const { width, height } = primaryDisplay.workAreaSize;
+    createWindow(width, height);
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (!isMac) {
-    app.quit();
-  }
+    if (!isMac) {
+        app.quit();
+    }
 });
 
 try {
-  require('electron-reloader')(module);
+    require('electron-reloader')(module);
 } catch (_) {}
--- renderer/js/index.js
+++ renderer/js/index.js
@@ -4,55 +4,48 @@ const { ipcRenderer } = require('electron');
 const jQuery = require('jquery');
 
 document.addEventListener('DOMContentLoaded', () => {
-  jQuery('#startBtn').on('click', function (e) {
-    ipcRenderer.on('fetch-data-response', (event, questionsArr) => {
-      // JSON.parse(Buffer.from(data).toString('utf8'));
+    jQuery('#startBtn').on('click', function (e) {
+        ipcRenderer.on('fetch-data-response', (event, questionsArr) => {
+            // JSON.parse(Buffer.from(data).toString('utf8'));
 
-      jQuery(this).addClass('animated fadeOutUp');
-      setTimeout(() => {
-        jQuery(this).hide();
-        jQuery('header h1').hide();
-      }, 300);
+            jQuery(this).addClass('animated fadeOutUp');
+            setTimeout(() => {
+                jQuery(this).hide();
+                jQuery('header h1').hide();
+            }, 300);
 
-      jQuery('.quiz').addClass('animated fadeInDown').removeClass('hidden');
+            jQuery('.quiz').addClass('animated fadeInDown').removeClass('hidden');
+            jQuery('form').addClass('animated fadeInDown').prepend(buildQuestionTemplate(0));
 
-      jQuery('form')
-        .addClass('animated fadeInDown')
-        .prepend(buildQuestionTemplate(0));
+            let correctAnswersCount = 0;
+            (function checkSelectedAnswer() {
+                jQuery('.btnQ').on('click', function (e) {
+                    const btnQLabel = jQuery(this);
+                    const selectedAnswer = btnQLabel.attr('for');
+                    let index = btnQLabel.parent().parent().data('index');
 
-      let correctAnswersCount = 0;
-      (function checkSelectedAnswer() {
-        jQuery('.btnQ').on('click', function (e) {
-          const selectedAnswer = jQuery(this).attr('for');
-          let index = jQuery(this).parent().parent().data('index');
+                    if (questionsArr[index].correct_answers[selectedAnswer + '_correct'] === 'true') {
+                        correctAnswersCount++;
+                    }
+                    btnQLabel.parent().parent().addClass('animated fadeOutUp').hide();
+                    const nextQuestionIndex = ++index;
 
-          if (
-            questionsArr[index].correct_answers[selectedAnswer + '_correct'] ===
-            'true'
-          ) {
-            correctAnswersCount++;
-          }
-          jQuery(this).parent().parent().addClass('animated fadeOutUp').hide();
-          const nextQuestionIndex = ++index;
+                    circle.animate(nextQuestionIndex / questionsArr.length);
 
-          circle.animate(nextQuestionIndex / questionsArr.length);
+                    // Show next question if any
+                    if (questionsArr[nextQuestionIndex]) {
+                        jQuery('form').addClass('animated fadeInDown').prepend(buildQuestionTemplate(nextQuestionIndex));
+                        checkSelectedAnswer();
+                    } else {
+                        showResult(index, correctAnswersCount);
+                    }
+                });
+            })();
 
-          // Show next question if any
-          if (questionsArr[nextQuestionIndex]) {
-            jQuery('form')
-              .addClass('animated fadeInDown')
-              .prepend(buildQuestionTemplate(nextQuestionIndex));
-            checkSelectedAnswer();
-          } else {
-            showResult(index, correctAnswersCount);
-          }
-        });
-      })();
+            function buildQuestionTemplate(index) {
+                const question = questionsArr[index];
 
-      function buildQuestionTemplate(index) {
-        const question = questionsArr[index];
-
-        return `<fieldset data-index=${index} class="q q${index}">
+                return `<fieldset data-index=${index} class="q q${index}">
                       <div class="content">
                           <legend>1. ${question.question}</legend>
                           <input type="radio" name="q${index}" id="${question.answers.answer_a}" class="btnQ">
@@ -68,53 +61,53 @@ document.addEventListener('DOMContentLoaded', () => {
   
                       </div>
                   </fieldset>`;
-      }
+            }
 
-      function showResult(index, correctAnswersCount) {
-        jQuery('.q' + index).addClass('animated fadeOutUp');
+            function showResult(index, correctAnswersCount) {
+                jQuery('.q' + index).addClass('animated fadeOutUp');
 
-        let message;
-        if (correctAnswersCount === questionsArr.length) {
-          message =
-            'Congratulations on completing the code quiz game with an impressive score! Your knowledge and dedication have shone through, making you a true quiz master.';
-        } else if (correctAnswersCount === 0) {
-          message =
-            "Keep up the effort! While you may not have scored any correct answers in the code quiz game, your participation is valuable and provides an opportunity for personal growth. Don't be discouraged, and continue to embrace challenges as a chance to learn and improve.";
-        } else {
-          message =
-            'Fantastic work on the code quiz game! Your partially correct answers showcase your knowledge and dedication in tackling the challenging questions. Keep up the excellent work and continue expanding your understanding in various coding subjects.';
-        }
+                let message;
+                if (correctAnswersCount === questionsArr.length) {
+                    message =
+                        'Congratulations on completing the code quiz game with an impressive score! Your knowledge and dedication have shone through, making you a true quiz master.';
+                } else if (correctAnswersCount === 0) {
+                    message =
+                        "Keep up the effort! While you may not have scored any correct answers in the code quiz game, your participation is valuable and provides an opportunity for personal growth. Don't be discouraged, and continue to embrace challenges as a chance to learn and improve.";
+                } else {
+                    message =
+                        'Fantastic work on the code quiz game! Your partially correct answers showcase your knowledge and dedication in tackling the challenging questions. Keep up the excellent work and continue expanding your understanding in various coding subjects.';
+                }
 
-        jQuery('.quiz').append(
-          `<div class="end animated bounceInDown">${message} <br />
+                jQuery('.quiz').append(
+                    `<div class="end animated bounceInDown">${message} <br />
               Your score: (${correctAnswersCount} / ${questionsArr.length})</div>
             <div class="buttons">
               <button class="btn-green" id="reloadBtn">
                 <img class="icon" src="../../resources/reload-6x-white.png" /> Try again
               </button>
             </div>
-          `,
-        );
+          `
+                );
 
-        jQuery('#reloadBtn').on('click', () => ipcRenderer.send('reload'));
-      }
+                jQuery('#reloadBtn').on('click', () => ipcRenderer.send('reload'));
+            }
+        });
+
+        ipcRenderer.send('fetch-data');
     });
-
-    ipcRenderer.send('fetch-data');
-  });
-  const circle = new ProgressBar.Circle('#circle-container', {
-    duration: 200,
-    strokeWidth: 5,
-    trailWidth: 5,
-    trailColor: '#ddd',
-    from: {
-      color: '#218CCC',
-    },
-    to: {
-      color: '#047E3C',
-    },
-    step: function (state, circle) {
-      circle.path.setAttribute('stroke', state.color);
-    },
-  });
+    const circle = new ProgressBar.Circle('#circle-container', {
+        duration: 200,
+        strokeWidth: 5,
+        trailWidth: 5,
+        trailColor: '#ddd',
+        from: {
+            color: '#218CCC',
+        },
+        to: {
+            color: '#047E3C',
+        },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+        },
+    });
 });
